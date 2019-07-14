52cda5cceb9bc0a8ec0679d8dd8928ad259d401d
dashuleka01 <dashka01012000@mail.ru>
2019-07-14 23:23:16 +0300

    Create PersonalAreaOrg.js

---
+++ PersonalAreaOrg.js
@@ -0,0 +1,18 @@
+function SubmitOnMouseOver(elmnt) {
+  elmnt.style.background = '#2ca97d';
+  elmnt.style.color = 'white';
+}
+
+function SubmitOnMouseOut(elmnt) {
+  elmnt.style.background = 'white';
+  elmnt.style.color = '#2ca97d';
+}
+
+function UpOnMouseOver(elmnt) {
+  elmnt.style.opacity = 1;
+}
+
+function UpOnMouseOut(elmnt) {
+  elmnt.style.opacity = .5;
+}
+
