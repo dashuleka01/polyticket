ea4962d0d06be8e508b3f6f86dab15732ef9204c
dashuleka01 <dashka01012000@mail.ru>
2019-07-20 04:56:32 +0300

    Update PersonalAreaOrg.js

--- PersonalAreaOrg.js
+++ PersonalAreaOrg.js
@@ -8,9 +8,28 @@ function SubmitOnMouseOut(elmnt) {
   elmnt.style.color = '#2ca97d';
 }
 
+function SubmitOnMouseOnClick(elmnt) {
+  var Dialog = document.getElementById('saveorno');
+  Dialog.showModal();
+}
+
+function YesButtonOnClick(){
+	var Dialog = document.getElementById('saveorno');
+    Dialog.close();
+	
+	var labelSaved = document.getElementById('saved');
+	labelSaved.style.visibility = 'visible';
+}
+
+function CancelButtonOnClick(elmnt){
+	var Dialog = document.getElementById('saveorno');
+    Dialog.close();
+}
+
 function UpOnMouseOver(elmnt) {
   elmnt.style.opacity = 1;
 }
+
 
 function UpOnMouseOut(elmnt) {
   elmnt.style.opacity = .5;
